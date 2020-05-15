15912e121b65a103611d4037e401ffd3f4d7808f
Nick <sonterixo@gmail.com>
2020-05-15 12:22:43 +0300

    jsconfig for base path

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './App.jsx';
-import store from './store/store.js';
-import './index.scss';
+import App from 'App.jsx';
+import store from 'store/store.js';
+import 'index.scss';
 
 ReactDOM.render(
   <Provider store={ store }>
