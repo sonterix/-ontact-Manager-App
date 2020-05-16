4aba10c254355d3ded1fd4d02ed88da2e3d5bfc1
Nick <sonterixo@gmail.com>
2020-05-17 02:02:10 +0300

    404 page

--- src/Router.js
+++ src/Router.js
@@ -10,6 +10,7 @@ const Logout = lazy(() => import('containers/Logout/LogoutContainer'))
 const Contacts = lazy(() => import('containers/Contacts/ContactsContainer'))
 const ContactDetails = lazy(() => import('containers/ContactDetails/ContactDetailsContainer'))
 const ContactCreate = lazy(() => import('containers/ContactCreate/ContactCreateContainer'))
+const NoFound = lazy(() => import('components/NoFound/NoFound'))
 
 const PrivateRoute = ({ component: Component, ...props }) => {
   const isLogged = useSelector(({ login: { isLogged } }) => isLogged)
@@ -35,10 +36,10 @@ const Router = () => (
           <Route path="/" component={ Home } exact />
           <Route path="/login" component={ Login } exact />
           <Route path="/logout" component={ Logout } exact />
-          <PrivateRoute path={[ '/contacts/:pageId', '/contacts' ]} component={ Contacts } />
-          <PrivateRoute path="/contact-details/:userId" component={ ContactDetails } />
-          <PrivateRoute path="/contact-create" component={ ContactCreate } />
-          
+          <PrivateRoute path={[ '/contacts/:pageId', '/contacts' ]} component={ Contacts } exact />
+          <PrivateRoute path="/contact-details/:userId" component={ ContactDetails } exact />
+          <PrivateRoute path="/contact-create" component={ ContactCreate } exact />
+          <Route component={ NoFound } exact />
         </Switch>
       </Suspense>
     </App>
