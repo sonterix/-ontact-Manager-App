642c609848c7394921a86c4e42813d4d4b22c960
Nick <sonterixo@gmail.com>
2020-05-17 02:34:16 +0300

    UI improvments

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react'
-import PropTypes from 'prop-types'
-import { auth, checkForEmptyInputs } from 'helpers.js'
 import { useHistory, Redirect } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faKeyboard } from '@fortawesome/free-solid-svg-icons'
+import { auth, checkForEmptyInputs } from 'helpers.js'
 import styles from './Login.module.scss'
 
 const Login = ({ isLogged, loginUser, loadingOn, loadingOff, setAlert, unsetAlert }) => {
@@ -17,8 +19,13 @@ const Login = ({ isLogged, loginUser, loadingOn, loadingOff, setAlert, unsetAler
 
   const handleInputData = ({ target: { name, value } }) => setFormData({ ...formData, [name]: value }) 
 
+  const handleFillForm = () => {
+    setFormData({ ...formData, email: 'michael.lawson@reqres.in', password: 'StrongTurtle' })
+  }
+
   const handleSubmitForm = async event => {
     event.preventDefault()
+
     // just base validation to not spend so much time
     const inputErrors = checkForEmptyInputs([ emailInput.current, passwordInput.current ])
 
@@ -57,14 +64,19 @@ const Login = ({ isLogged, loginUser, loadingOn, loadingOff, setAlert, unsetAler
           <div className={ `wrapper ${ styles.LoginFormWrapper }` }>
             <h1 className={ styles.LoginFormTitle }>Login</h1>
             <form className={ styles.LoginForm } onSubmit={ event => handleSubmitForm(event) }>
-              <input
-                type="email"
-                name="email"
-                placeholder="Your Email"
-                ref={ emailInput }
-                value={ email }
-                onChange={ event => handleInputData(event) }
-              />
+              <div className={ styles.Hint }>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Your Email"
+                  ref={ emailInput }
+                  value={ email }
+                  onChange={ event => handleInputData(event) }
+                />
+                <button type="button" onClick={ () => handleFillForm() }>
+                  <FontAwesomeIcon icon={ faKeyboard } />
+                </button>
+              </div>
 
               <input
                 type="password"
