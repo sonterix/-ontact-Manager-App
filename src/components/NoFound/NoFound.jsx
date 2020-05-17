d0128bb9225ef6929035e088556108aaf80d3085
Nick <sonterixo@gmail.com>
2020-05-17 04:07:45 +0300

    minimize images and dark 404

--- src/components/NoFound/NoFound.jsx
+++ src/components/NoFound/NoFound.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { LazyLoadImage } from 'react-lazy-load-image-component'
-import noFount from 'assets/images/404.png'
 import styles from './NoFound.module.scss'
 
 const NoFound = () => (
   <div className={ styles.NoFound }>
-    <LazyLoadImage src={ noFount } alt="404" effect="blur" />
+    <div className={ styles.NoFoundImage }></div>
   </div>
 )
 
